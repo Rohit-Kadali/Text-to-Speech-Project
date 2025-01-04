6fecce97c531e51948e467efbe44ec9895ad8adf
Rohit Sri Surya Kadali <krohithsrisurya@gmail.com>
2025-01-04 19:42:32 +0530

    Add files via upload

---
+++ Text to Speech/main.py
@@ -0,0 +1,8 @@
+from gtts import gTTS
+import os
+
+mytext = " Aspiring Developer on Full Stack web development Using Python Django and really curious about tech and programming My Skills are Python, Django, HTML, CSS, JavaScript, jQuery and Bootstrap"
+language = "en"
+myobj = gTTS(text=mytext, lang=language, slow=False)
+myobj.save("result.mp3")
+os.system("result.mp3")
