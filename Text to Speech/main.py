ab2cb8b67039d7b0b1538c232e7124f7b7287ab7
Rohit Sri Surya Kadali <krohithsrisurya@gmail.com>
2025-01-04 19:46:22 +0530

    Update main.py

--- Text to Speech/main.py
+++ Text to Speech/main.py
@@ -1,7 +1,7 @@
 from gtts import gTTS
 import os
 
-mytext = " Aspiring Developer on Full Stack web development Using Python Django and really curious about tech and programming My Skills are Python, Django, HTML, CSS, JavaScript, jQuery and Bootstrap"
+mytext = " Hi Myself Rohit Sri Surya and I'm Aspiring Developer on Full Stack web development Using Python Django and really curious about tech and programming My Skills are Python, Django, HTML, CSS, JavaScript, jQuery and Bootstrap" and looking forward to meet you
 language = "en"
 myobj = gTTS(text=mytext, lang=language, slow=False)
 myobj.save("result.mp3")
